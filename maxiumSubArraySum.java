e533518923c1ef2976d67f025a605bd05a4229a1
SATYARTH <satyarthtiwari123@gmail.com>
2022-08-25 18:02:14 +0530

    LEETCODE 53

---
+++ maxiumSubArraySum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+    for(int i = 0;i<nums.length;i++){
+        
+        currSum = currSum+nums[i];
+    
+        currSum = Math.max(currSum,nums[i]);
+		
+	    maxSum = Math.max(currSum,maxSum);
+    }
+    return maxSum;
+    
+    }
+}
