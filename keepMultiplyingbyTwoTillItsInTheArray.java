5932b80b7f4ff6ccad9fbf19eb265f6c8a2c702a
SATYARTH <satyarthtiwari123@gmail.com>
2023-02-13 22:42:58 +0530

    LEETCODE 2154

---
+++ keepMultiplyingbyTwoTillItsInTheArray.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Arrays.sort(nums);
+        while(Arrays.binarySearch(nums,original)>=0) original*=2;
+        return original;
+        
+    }
+}
