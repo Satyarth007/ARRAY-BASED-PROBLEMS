555dbe9557829a12c090fb00fa5185ac9b9df203
SATYARTH <satyarthtiwari123@gmail.com>
2023-02-21 21:48:29 +0530

    LEETCODE 1588

---
+++ sumOfAllOddLengthSubarrays.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                for(int k=i;k<=j;k++){
+                    if(i==j || (j-i)%2==0) sum+=arr[k];
+                }
+
+            }
+        }
+        return sum;
+        
+    }
+}
